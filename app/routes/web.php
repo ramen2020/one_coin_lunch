500558edf01b9e62dd3e5248675a9a7b4a3f6988
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-08-12 22:39:06 +0900

    add_ルーティング追加

--- app/routes/web.php
+++ app/routes/web.php
@@ -17,12 +17,40 @@ Auth::routes();
 // topページ
 Route::get('/', 'RestaurantController@index')->name('restaurant.index');
 
+// 絞り込み検索
+Route::group(['prefix' => 'filter'], function () {
+    Route::get('/', function () {
+        return view('search.filter');
+    });
+    Route::get('/search', 'SearchController@word')->name('search.filter');
+});
+
+// 地図検索
+Route::group(['prefix' => 'map'], function () {
+    Route::get('/', function () {
+        return view('search.map');
+    });
+    Route::get('/search', 'SearchController@map')->name('search.map');
+});
+
+// キーワードで検索
+Route::group(['prefix' => 'word'], function () {
+    Route::get('/', function () {
+        return view('search.word');
+    });
+    Route::get('/search', 'SearchController@word')->name('search.word');
+});
+
+
+
+// twitter ログイン
 Route::get('login/twitter', 'Auth\TwitterLoginController@redirectToProvider')->name('login.twitter');
 Route::get('auth/twitter/callback', 'Auth\TwitterLoginController@handleProviderCallback');
 Route::get('auth/twitter/logout', 'Auth\TwitterController@logout');
 
 Route::group(['middleware' => 'auth'], function () {
 
+    // お問い合わせ
     Route::group(['prefix' => 'contact'], function () {
         //入力ページ
         Route::get('/', 'ContactController@index')->name('contact.index');
@@ -32,6 +60,7 @@ Route::group(['middleware' => 'auth'], function () {
         Route::post('/thanks', 'ContactController@send')->name('contact.send');
     });
 
+    // ワンコインランチの店舗について
     Route::group(['prefix' => 'restaurant'], function () {
         // 新規作成画面
         Route::get('/create', 'RestaurantController@create')->name('restaurant.create');
@@ -40,11 +69,22 @@ Route::group(['middleware' => 'auth'], function () {
         // 登録
         Route::post('/store', 'RestaurantController@store')->name('restaurant.store');
         // 編集画面
-        Route::get('/edit/{restaurant_id}', 'RestaurantController@confirm')->name('restaurant.edit');
+        Route::get('/edit/{restaurant_id}', 'RestaurantController@edit')->name('restaurant.edit');
         // 更新
         Route::put('/update/{restaurant_id}', 'RestaurantController@update')->name('restaurant.update');
         // 詳細
         Route::get('/show/{restaurant_id}', 'RestaurantController@show')->name('restaurant.show');
+    });
+
+    // プロフィールについて
+    Route::group(['prefix' => 'user'], function () {
+        // 編集画面
+        Route::get('/edit/myProfile', 'UserController@editMyProfile')->name('user.editMyProfile');
+        // 更新
+        Route::put('/update', 'UserController@update')->name('user.update');
+        // 詳細
+        Route::get('/profile/{user_id}', 'UserController@profile')->name('user.profile');
+        Route::get('/myProfile', 'UserController@myProfile')->name('user.myProfile');
     });
 
     Route::get('/home', 'HomeController@index')->name('home');
