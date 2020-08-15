81ac9213eaffd962a2b1f3473246d489c008edf0
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-08-15 17:14:51 +0900

    fix_productionカラムとprofile_imageカラムにnullを許容するように修正

--- app/database/migrations/2014_10_12_000000_create_users_table.php
+++ app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,10 +18,9 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->bigInteger('twitter_id')->nullable()->comment('twitterのid');
             $table->string('twitter_nickname')->nullable()->comment('twitterの名前');
-            $table->string('twitter_avator')->nullable()->comment('twitterの画像');
-            $table->string('name')->unique()->comment('名前');
-            $table->string('introduction')->comment('自己紹介');
-            $table->string('profile_image')->comment('自己紹介');
+            $table->string('name')->comment('名前');
+            $table->string('introduction')->nullable()->comment('自己紹介');
+            $table->string('profile_image')->nullable()->comment('画像');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
