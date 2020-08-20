aa5394aa8821260e5c776a117815b68edd853508
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-08-20 21:47:01 +0900

    add_新着一覧ページを作成

--- app/app/Http/Controllers/RestaurantController.php
+++ app/app/Http/Controllers/RestaurantController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Restaurant;
+use App\Favorite;
 use App\Services\RestaurantService;
 use App\Http\Requests\Restaurant\StoreRestaurantRequest;
 use Illuminate\Http\Request;
@@ -10,16 +11,42 @@ use Illuminate\Http\Request;
 class RestaurantController extends Controller
 {
     private $restaurant_service;
+    private $favorite;
+    private $category_list;
 
-    public function __construct(RestaurantService $restaurant_service)
+    public function __construct(RestaurantService $restaurant_service, Favorite $favorite)
     {
         $this->restaurant_service = $restaurant_service;
+        $this->favorite = $favorite;
+        $this->category_list = config('data.category');
+    }
+
+    public function newRestaurants()
+    {
+        $category_list = $this->category_list;
+        $restaurants = Restaurant::with(['user','favorites'])
+            ->orderby('restaurants.created_at', 'DESC')
+            ->paginate(6);
+
+        $restaurants = $restaurants->toArray();
+
+        foreach($restaurants['data'] as &$restaurant) {
+            foreach($restaurant['favorites'] as $favorite) {
+                if($favorite['user_id'] == \Auth::id()) {
+                    $restaurant['is_favorite'] = true;
+                    $restaurant['favorite_id_by_auth'] = $favorite['id'];
+                }
+            }
+        }
+
+        unset($restaurant);
+
+        return response()->json(['restaurants' => $restaurants, 'catogory_list' => $category_list]);
     }
 
     public function index()
     {
-        $restaurants = $this->restaurant_service->getAllRestaurants();
-        return view('top.top' , compact('restaurants'));
+        return view('top.top');
     }
 
 
@@ -50,7 +77,23 @@ class RestaurantController extends Controller
 
     public function show($id)
     {
+        $user = \Auth::user();
+        $favorite_id = @$this->favorite->getFavoriteId($user->id, $id)->id ?: null;
         $restaurant = $this->restaurant_service->getRestaurantById($id);
-        return view('restaurant.detail', compact('restaurant'));
+        return view('restaurant.detail', compact('user', 'favorite_id', 'restaurant'));
+    }
+
+    public function favoriteList()
+    {
+        $user = \Auth::user();
+        $favorite_restaurants = \Auth::user()->favorites()->with('restaurant')->get();
+        $restaurants = [];
+
+        foreach($favorite_restaurants as $favorite) {
+            $favorite->restaurant['favorite_id'] = $favorite['id'];
+            array_push($restaurants, $favorite->restaurant);
+        }
+
+        return view('restaurant.favorite', compact('restaurants'));
     }
 }
---
+++ app/resources/views/restaurant/new.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+
+<!-- <img class="img-fluid mb-4" src="image/morning-brew-eFSUPUeYs3w-unsplash.jpg" alt="1200❌300"> -->
+
+<div class="container">
+
+    <h4 class="mt-4 mb-3">
+        近くのワンコインランチを探そう
+    </h4>
+    <div class="row mt-10">
+        {!! Form::open(['route' => 'search.word', 'method' => 'GET', 'class' => 'input-group mb-5']) !!}
+            {!! Form::input('text', 'word', null, ['class' => 'border-secondary form-control', 'placeholder' => '店舗名とか場所とか...']) !!}
+            {!! Form::button('検索', ['class' => 'btn border-secondary', 'type' => 'submit'])!!}
+        {!! Form::close() !!}
+    </div>
+
+    <div class="row">
+
+        <div class="col-lg-2 mb-4">
+            @include('sidebar.sidebar')
+        </div>
+
+        <div class="col-lg-9">
+            <div class="new-restaurants">
+                <div class="row">
+                    @if(Auth::user())
+                        <restaurants-list-component user-id="{{ Auth::id() }}"></restaurants-list-component>
+                    @else
+                        <restaurants-list-component></restaurants-list-component>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
