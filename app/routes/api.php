8991aaad360a93a0c9d7790fe8714856593c9cc6
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-08-20 21:48:07 +0900

    add_ユーザー画像編集のルーティングを追加

--- app/routes/api.php
+++ app/routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/user/image', 'UserController@updateImage')->name('user.image.update');
