0c7c9cf91c8b8671d99259898bde34811103cc52
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-08-20 21:46:14 +0900

    fix_topページに店舗コンポーネントを使用するように修正

--- app/resources/views/top/top.blade.php
+++ app/resources/views/top/top.blade.php
@@ -9,11 +9,12 @@
     <h4 class="mt-4 mb-3">
         近くのワンコインランチを探そう
     </h4>
-
-    {!! Form::open(['route' => 'search.word', 'method' => 'GET', 'class' => 'input-group mb-5']) !!}
-        {!! Form::input('text', 'word', null, ['class' => 'border-secondary form-control', 'placeholder' => '店舗名とか場所とか...']) !!}
-        {!! Form::button('検索', ['class' => 'btn input-group-append border-secondary', 'type' => 'submit'])!!}
-    {!! Form::close() !!}
+    <div class="row mt-10">
+        {!! Form::open(['route' => 'search.word', 'method' => 'GET', 'class' => 'input-group mb-5']) !!}
+            {!! Form::input('text', 'word', null, ['class' => 'border-secondary form-control', 'placeholder' => '店舗名とか場所とか...']) !!}
+            {!! Form::button('検索', ['class' => 'btn border-secondary', 'type' => 'submit'])!!}
+        {!! Form::close() !!}
+    </div>
 
     <div class="row">
 
@@ -33,57 +34,12 @@
 
             <div class="new-restaurants">
                 <div class="row">
-                    <h2 class="border-bottom col-lg-12 my-3">新着投稿一覧</h2>
-                    @foreach($restaurants as $restaurant)
-                        <div class="col-lg-4 mt-2">
-                            <div class="card h-100">
-                                <a href="{{ $restaurant['image_name'] }}">
-                                    <img class="card-img-top" src="image/morning-brew-eFSUPUeYs3w-unsplash.jpg"
-                                        alt="ワンコインランチの画像">
-                                </a>
-                                <div class="card-body">
-                                    <h4>
-                                        <a href="/restaurant/show/{{ $restaurant['id'] }}">{{ $restaurant['store_name'] }}</a>
-                                    </h4>
-                                    <p>
-                                        {{ $restaurant['high_budget'] }}円~{{ $restaurant['low_budget'] }}円
-                                    </p>
-                                    <p>
-                                        {{ $restaurant['address'] }}
-                                    </p>
-                                    <div class="text-right">
-                                        <a href="{{ route('user.profile', $restaurant->user->id) }}">{{ $restaurant->user->name }}</a>
-                                    </div>
-                                    <div>
-                                        @if(!empty($restaurant['category_id_1']))
-                                            <v-btn color="primary" href="{{ route('search.category', $restaurant['category_id_1']) }}"
-                                                class="m-1">{{ config('data.category')[$restaurant['category_id_1']] }}</v-btn>
-                                        @endif
-                                        @if(!empty($restaurant['category_id_2']))
-                                            <v-btn color="primary" href="{{ route('search.category', $restaurant['category_id_2']) }}"
-                                                　class="m-1">{{ config('data.category')[$restaurant['category_id_2']] }}</v-btn>
-                                        @endif
-                                        @if(!empty($restaurant['category_id_3']))
-                                            <v-btn color="primary" href="{{ route('search.category', $restaurant['category_id_3']) }}"
-                                                　class="m-1">{{ config('data.category')[$restaurant['category_id_3']] }}</v-btn>
-                                        @endif
-                                        @if(!empty($restaurant['category_id_4']))
-                                            <v-btn color="primary" href="{{ route('search.category', $restaurant['category_id_4']) }}"
-                                                class="m-1">{{ config('data.category')[$restaurant['category_id_4']] }}</v-btn>
-                                        @endif
-                                        @if(!empty($restaurant['category_id_5']))
-                                            <v-btn color="primary" href="{{ route('search.category', $restaurant['category_id_5']) }}"
-                                                class="m-1">{{ config('data.category')[$restaurant['category_id_5']] }}</v-btn>
-                                        @endif
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    @endforeach
+                    @if(Auth::user())
+                        <restaurants-list-component user-id="{{ Auth::id() }}"></restaurants-list-component>
+                    @else
+                        <restaurants-list-component></restaurants-list-component>
+                    @endif
                 </div>
-            </div>
-            <div class="my-4 d-flex justify-content-center">
-                {{ $restaurants->links() }}
             </div>
         </div>
     </div>
