0830d635455e929b6e91bdf30b2d9d36e6adab52
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-08-27 22:02:41 +0900

    fix_route

--- app/routes/web.php
+++ app/routes/web.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 Auth::routes();
 
 // guestユーザー ログイン
